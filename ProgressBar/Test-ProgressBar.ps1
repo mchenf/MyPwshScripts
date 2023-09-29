a07c54d114ab403c4af8f5260dd1c4370c496d22
mchenf <mochen@foss.dk>
2023-09-29 15:27:13 +0200

    Refined the code

--- ProgressBar/Test-ProgressBar.ps1
+++ ProgressBar/Test-ProgressBar.ps1
@@ -6,7 +6,6 @@ $d = 0;
 [string]$outputString = [string]::Concat([string]::new($tobe, $sum - $d), "`r")
 Write-Host $outputString -NoNewLine
 
-[int]$progress = 0;
 [System.Random]$rnd = [System.Random]::new();
 [int]$r = $rnd.Next(50, 377);
 while ($d -lt $sum) {
